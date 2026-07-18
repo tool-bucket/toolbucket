435741073a2790a78925ee334098431eec2f7654
tool-bucket <vlogsnitinsaini@gmail.com>
2026-07-18 13:47:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -148,3 +148,45 @@ document.addEventListener("click", ()=>{
     toolsDropdown.classList.remove("active");
 
 });
+
+
+
+/* =========================
+      Mobile Menu
+========================= */
+
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+const overlay = document.querySelector(".overlay");
+
+menuToggle.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+
+    document.body.classList.toggle("menu-open");
+
+});
+
+overlay.addEventListener("click", () => {
+
+    mobileMenu.classList.remove("active");
+    overlay.classList.remove("active");
+
+    document.body.classList.remove("menu-open");
+
+});
+
+
+/* =========================
+    Mobile Dropdown
+========================= */
+
+const mobileTools = document.querySelector(".mobile-tools");
+const mobileToolsBtn = document.querySelector(".mobile-tools-btn");
+
+mobileToolsBtn.addEventListener("click", () => {
+
+    mobileTools.classList.toggle("active");
+
+});
